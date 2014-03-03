ea31dea5495a7866ea9e03b9c6c983f7cc254812
Ben Aston <ben@bj.ma>
2014-03-03 14:59:06 +0000

    foo

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      dist: {
+        options: {
+          separator: ';'
+        },
+        src: [
+          'public/javascripts/**/*.js'
+        ],
+        dest: 'public/javascripts/<%= pkg.name %>.js'
+      },
+      css: {
+        src: [
+          'public/stylesheets/*.css'
+        ],
+        dest: 'public/stylesheets/<%= pkg.name %>.css'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+        preserveComments: false
+      },
+      dist: {
+        files: {
+          'public/javascripts/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    cssmin: {
+      css: {
+        src: 'public/stylesheets/<%= pkg.name %>.css',
+        dest: 'public/stylesheets/<%= pkg.name %>.min.css'
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'qunit']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-css');
+
+  grunt.registerTask('test', ['jshint', 'qunit']);
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
+};
